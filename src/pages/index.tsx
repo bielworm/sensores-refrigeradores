4ffcf4d35ed0d3f83a63fd2888fae2f4233697c7
Gabriel Assis <43077215+bielworm@users.noreply.github.com>
2024-11-05 18:28:44 -0300

    ui: add icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,32 @@ import Container from '../components/Partials/Container'
 import SensorMonitor from '../components/SensorMonitor'
 
 const Home: NextPage = () => {
-
   return (
     <div className="bg-gradient-to-tl from-[#141414] to-[#272727] w-full h-screen flex items-center justify-center bg-cover">
       <Container>
         <div className="shadow-sm shadow-zinc-200 p-8 w-full rounded-lg backdrop-blur">
-          <h2 className="font-thin underline text-white mb-4 text-center">Sensores - Refrigeradores</h2>
+          <div className="flex flex-row justify-center items-start gap-2">
+            <h2 className="font-thin underline text-white mb-4 text-center">
+              Sensores - Refrigeradores
+            </h2>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+            >
+              <g
+                fill="none"
+                stroke="#FFF"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+              >
+                <path d="M6 12a5 5 0 1 0 6 0m-6 0V3h6v9m0-9h2m-2 3h2m-2 3h2m5-2a2 2 0 1 0 0-4a2 2 0 0 0 0 4" />
+                <path d="M9 14a2 2 0 1 0 0 4a2 2 0 0 0 0-4m0 0v-3" />
+              </g>
+            </svg>
+          </div>
           <SensorMonitor />
         </div>
       </Container>
